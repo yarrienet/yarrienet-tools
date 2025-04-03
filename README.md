c57a8c6a12cb290561490b05f70f0bf8ae88a595
yarrie <yarrie@yarrie.net>
2025-04-03 16:33:38 +0100

    feat: cli complete for insertdates

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Repository containing tools used to maintain [yarrie.net](http://yarrie.net).
 
 Most of the tools are specialized and built to complete a highly specific task, however most of the code outside of `tools`, `scripts`, and `main.go` is generic and potentially helpful for any project.
 
+### Tools
+
+#### `insertdates.go`
+
+The tool uses the microblog's RSS feed to extract the date of each post and then insert said date semantically into the HTML tree. Created in order to migrate from using a static site generator backed by a database to a semantically storing data within the page's source.
+
+```sh
+insertdates <microblog file> <rss file> [output]
+```
+
--- tools/insertdates.go
+++ tools/insertdates.go
@@ -3,8 +3,8 @@ package main
 import (
     "yarrienet/htmlhelper"
     "yarrienet/rsshelper"
-    "fmt"
     "golang.org/x/net/html"
+    "fmt"
     "os"
     "strings"
     "slices"
@@ -16,42 +16,56 @@ func insertDateNodes(doc *html.Node, dates map[string]time.Time) *html.Node {
     // tracks the current id of the post
     var postId string
     // if node is currently nested within <div class="date">
-    var parentIsDate = false
+    var nestedInDateDiv = false
+    // if a time element has already been encountered before the p tag,
+    // don't add another
+    var existingTimeElement = false
     htmlhelper.WalkHtmlDoc(doc, func(wrappedNode *htmlhelper.NodeWrapper, event htmlhelper.WalkEvent) bool {
         if wrappedNode.Type == "div" {
             if slices.Contains(wrappedNode.Classes, "post") {
                 if event == htmlhelper.WalkEnter {
+                    // entering a new post
                     postId = wrappedNode.ID
                 } else {
+                    // exiting a new post, reset state
                     postId = ""
                 }
             } else if slices.Contains(wrappedNode.Classes, "date") {
-                parentIsDate = event == htmlhelper.WalkEnter
+                nestedInDateDiv = event == htmlhelper.WalkEnter
+                if event == htmlhelper.WalkExit {
+                    // exiting a time element, reset state
+                    existingTimeElement = false
+                    return true
+                }
             }
         }
 
-        if postId != "" && parentIsDate && event == htmlhelper.WalkEnter && wrappedNode.Type == "p" {
-            // get the date
-            postDate, exists := dates[postId]
-            if !exists {
-                // if doesnt exist in map then skip date insert
-                return true
-            }
+        if postId != "" && nestedInDateDiv && event == htmlhelper.WalkEnter {
+            if wrappedNode.Type == "time" {
+                existingTimeElement = true
+            } else if wrappedNode.Type == "p" && !existingTimeElement {
+                // get the date
+                postDate, exists := dates[postId]
+                if !exists {
+                    // if doesnt exist in map then skip date insert
+                    return true
+                }
 
-            node := wrappedNode.Node
-            nodeParent := node.Parent
-            if nodeParent == nil {
-                return true
-            }
-            nodeParent.RemoveChild(node)
+                node := wrappedNode.Node
+                nodeParent := node.Parent
+                if nodeParent == nil {
+                    return true
+                }
+                nodeParent.RemoveChild(node)
 
-            dateNode := htmlhelper.MakeDateNode(postDate)
-            dateNode.AppendChild(node)
-            
-            if node.NextSibling != nil {
-                nodeParent.InsertBefore(&dateNode, node.NextSibling)
-            } else {
-                nodeParent.AppendChild(&dateNode)
+                dateNode := htmlhelper.MakeDateNode(postDate)
+                dateNode.AppendChild(node)
+                
+                if node.NextSibling != nil {
+                    nodeParent.InsertBefore(&dateNode, node.NextSibling)
+                } else {
+                    nodeParent.AppendChild(&dateNode)
+                }
             }
         }
 
@@ -74,9 +88,37 @@ func determineRssDates(data []byte) (map[string]time.Time, error) {
     return postDates, nil
 }
 
+const usageString = "insertdates <microblog file> <rss file> [output]"
+func printUsage() {
+    fmt.Println("USAGE")
+    fmt.Println("  " + usageString)
+    fmt.Println("")
+    fmt.Println("OUTPUT")
+    fmt.Println("  Wrap each yarrie.net microblog date <p> in <time datetime>")
+    fmt.Println("  sourced from a linked RSS feed.")
+    fmt.Println("")
+    fmt.Println("  If no output file is provided then stdout is used.")
+}
+
+func printMissingArg(arg string) {
+    fmt.Fprintln(os.Stderr, fmt.Sprintf("missing argument: %s", arg))
+    fmt.Fprintln(os.Stderr, fmt.Sprintf("usage: %s", usageString))
+}
+
 func main() {
+    if len(os.Args) < 2 {
+        printMissingArg("microblog file")
+        os.Exit(1)
+    }
+    if len(os.Args) < 3 {
+        printMissingArg("rss file")
+        os.Exit(1)
+    }
+    microblogFilePath := os.Args[1]
+    rssFilePath := os.Args[2]
+
     // 1. determine the dates for each id
-    rssFile, err := os.Open("rss.xml")
+    rssFile, err := os.Open(rssFilePath)
     if err != nil {
         panic(err)
     }
@@ -92,7 +134,7 @@ func main() {
     }
 
     // 2. update the dates for each post
-    f, err := os.Open("microblog.html")
+    f, err := os.Open(microblogFilePath)
     if err != nil {
         panic(err)
     }
@@ -105,12 +147,27 @@ func main() {
 
     doc = insertDateNodes(doc, dates)
 
-    var b strings.Builder
-    err = html.Render(&b, doc)
-    if err != nil {
-        panic(err)
-    }
+    if len(os.Args) > 3 {
+        // output into a file
+        outputFile, err := os.OpenFile(os.Args[3], os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+        if err != nil {
+            panic(err)
+        }
+        defer outputFile.Close()
 
-    fmt.Println(b.String())
+        err = html.Render(outputFile, doc)
+        if err != nil {
+            panic(err)
+        }
+    } else {
+        // output into stdout
+        var b strings.Builder
+        err = html.Render(&b, doc)
+        if err != nil {
+            panic(err)
+        }
+
+        fmt.Println(b.String())
+    }
 }
 
