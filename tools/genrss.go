4ffccdcb99e9c30794f3174efe0dbc654e361408
yarrie <yarrie@yarrie.net>
2025-04-04 10:24:06 +0100

    fix: optimized genrss walk

--- tools/genrss.go
+++ tools/genrss.go
@@ -15,52 +15,30 @@ func getNodeClasses(n *html.Node) []string {
 }
 
 func parseMicroblog(doc *html.Node) /*[]microblog.Post*/ {
-    //var postId string
-
-    // 1. walk down to find the posts div
+    var postId string
+    var nestedInPostDate = false
     htmlhelper.WalkHtmlDoc(doc, func (wn *htmlhelper.NodeWrapper, e htmlhelper.WalkEvent) bool {
-        if wn.ID == "posts" {
-            // 2. loop each post
-            n := wn.Node
-            for c := n.FirstChild; c != nil; c = c.NextSibling {
-                if c.Type != html.ElementNode {
-                    continue
-                }
-                var id string
-                var classes []string
-                for _, attr := range c.Attr {
-                    if attr.Key == "id" {
-                        id = attr.Val 
-                    } else if attr.Key == "class" {
-                        classes = strings.Fields(attr.Val)
-                    }
-                }
-                if c.Data == "div" && slices.Contains(classes, "post") {
-                    // 3. root level elements of each post
-                    fmt.Printf("got a post: %s\n", id)
-                    for c2 := c.FirstChild; c2 != nil; c2 = c2.NextSibling {
-                        if c2.Type != html.ElementNode {
-                            continue
-                        }
-                        classes = getNodeClasses(c2)
-                        if c2.Data == "div" && slices.Contains(classes, "date") {
-                            // 4. walk to find <time datetime>
-                            htmlhelper.WalkHtmlDoc(c2, func (wn2 *htmlhelper.NodeWrapper, e2 htmlhelper.WalkEvent) bool {
-                                if e2 == htmlhelper.WalkEnter && wn2.Type == "time" {
-                                    dateString := htmlhelper.GetNodeAttr(wn2.Node, "datetime")
-                                    fmt.Printf("date string: %s\n", dateString)
-                                    return false
-                                }
-                                return true
-                            })
-                        } else {
-                            // 5. else add the node to the post
-                            fmt.Printf("have a node: %s\n", c2.Data)
-                        }
-                    }
-                }
+        if postId == "" && slices.Contains(wn.Classes, "post") {
+            if e == htmlhelper.WalkEnter {
+                postId = wn.ID
+                fmt.Printf("have a post: %s\n", postId)
+            } else {
+                postId = ""
+                return false
             }
-            return false
+        } else if postId != "" {
+            if wn.Type == "div" && slices.Contains(wn.Classes, "date") {
+                nestedInPostDate = e == htmlhelper.WalkEnter
+            } else if nestedInPostDate {
+                if wn.Type == "time" {
+                    postDate := htmlhelper.GetNodeAttr(wn.Node, "datetime")
+                    fmt.Printf("post date: %s\n", postDate)
+                    return false
+                }
+            } else {
+                fmt.Printf("post node: '%s'\n", wn.Type)
+                return false 
+            }
         }
         return true
     })
